bd457442833de95240be96a080f79dd27c180a13
Anton I <AEIlin.SBT@sberbank.ru>
2017-02-04 17:15:23 +0300

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import * as typedoc from 'typedoc';
 import * as path from 'path';
 import startServer from './client';
 
-startServer();
+// startServer();
 
-// const app = new typedoc.Application();
+const app = new typedoc.Application();
 
-// app.generateJson([
-//     '../../ufs-ui-develop/src/common',
-//     '../../ufs-ui-develop/src/components'
-// ], './temp');
+app.generateJson([
+    '../../ufs-ui-develop/src/common',
+    '../../ufs-ui-develop/src/components'
+], './temp');
 
