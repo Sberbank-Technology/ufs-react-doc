e3388275aa3c5e3743be067b4a7346d3bd2a1d66
Nikolay Nadorichev <NVNadorichev.SBT@sberbank.ru>
2017-03-03 11:45:18 +0300

    Improved example

--- examples/simple/src/components/Anchor/Anchor.tsx
+++ examples/simple/src/components/Anchor/Anchor.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 
-export interface Props extends HTMLAnchorElement {
-    /* Link for an anchor */
+export interface Props {
+    /** Link for an anchor */
     href: string;
 }
 
 /** Simple component for anchors */
-export const Anchor = (props) => <a {...this.props}>{this.props.children}</a>;
-
-export default Anchor;
+export default class Anchor extends React.Component<Props, {}> {
+    render() {
+        return <a href={this.props.href}>{this.props.children}</a>;
+    }
+}
