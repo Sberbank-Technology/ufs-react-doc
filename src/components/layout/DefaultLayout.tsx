aa719d1e8394136b36c0c4c936f95095bcf00205
Nikolay Nadorichev <NVNadorichev.SBT@sberbank.ru>
2017-03-03 11:49:38 +0300

    layout fix

--- src/components/layout/DefaultLayout.tsx
+++ src/components/layout/DefaultLayout.tsx
@@ -8,12 +8,13 @@ export default function DefaultLayout(props: Props) {
     return (
         <html lang="en">
             <head>
-                <title>{props.title || "UFS React Doc"}</title>
+                <title>UFS React Doc</title>
                 <link rel="stylesheet" type="text/css" href="/bootstrap/css/bootstrap.min.css" />
             </head>
             <body>
                 <div className="container">
-                    <h1><img src="/public/UFS_logo.png" /> {props.title || "UFS React Doc"}</h1>
+                    <h1><img src="/public/UFS_logo.png" style={{width: 45, marginTop: -7}} /> UFS React Doc</h1>
+                    <hr />
                     {props.children}
                 </div>
             </body>
