2e3375728320d409363586e228a17b7b68dc56fb
Denis Smirnov <DSmirnov@at-consulting.ru>
2018-03-23 14:21:01 +0300

    Updated UFSError list

--- src/common/UFSError/index.tsx
+++ src/common/UFSError/index.tsx
@@ -2,10 +2,22 @@ import * as React from 'react';
 import { markdownToHtml } from '../../common/utils';
 import { PanelGroup, Panel, Table } from 'react-bootstrap';
 
-export default function UFSError(props: any) {
-    const { list } = props;
+export interface Props {
+    list: any;
+}
 
-    function makeObjectFromArray(array) {
+export interface State {
+    activeKey: any;
+    structuredObject: any;
+}
+
+export default class UFSError extends React.Component<Props, State> {
+    state = {
+        activeKey: 0,
+        structuredObject: {}
+    }
+
+    makeObjectFromArray(array) {
         let object = {} as any;
         array.forEach(element => {
             if (object[element.module] === undefined) {
@@ -19,7 +31,11 @@ export default function UFSError(props: any) {
         return object;
     }
 
-    function renderErrors(errors) {
+    componentDidMount() {
+        this.setState({ structuredObject: this.makeObjectFromArray(this.props.list) });
+    }
+
+    renderErrors(errors) {
         return (
             <Table striped bordered condensed hover>
                 <thead>
@@ -31,7 +47,7 @@ export default function UFSError(props: any) {
                 <tbody>
                     {Object.keys(errors).map((error, key) => (
                         <tr key={key}>
-                            <td>{error}</td>
+                            <td><code>{error}</code></td>
                             <td>{errors[error]}</td>
                         </tr>
                     ))}
@@ -40,36 +56,48 @@ export default function UFSError(props: any) {
         )
     }
 
-    function renderSubmodules(submodules) {
+    renderSubmodules(submodules) {
         return (
             <div>
                 {Object.keys(submodules).map((submodule, key) => (
-                    <div>
-                        <h5>{submodule}</h5>
-                        {renderErrors(submodules[submodule])}
+                    <div key={key}>
+                        <h4>
+                            {"submodule: "}
+                            <code>{submodule}</code>
+                        </h4>
+                        {this.renderErrors(submodules[submodule])}
                     </div>
                 ))}
             </div>
         )
     }
 
-    function renderModules() {
-        let structuredObject = makeObjectFromArray(list);
+    renderModules() {
         return (
-            <PanelGroup accordion>
-                {Object.keys(structuredObject).map((moduleKey, key) => (
-                    <Panel header={moduleKey} defaultExpanded >
-                        {renderSubmodules(structuredObject[moduleKey])}
+            <PanelGroup
+                activeKey={this.state.activeKey}
+                accordion>
+                {Object.keys(this.state.structuredObject).map((moduleKey, key) => (
+                    <Panel
+                        eventKey={key}
+                        header={"module: " + moduleKey}
+                        onSelect={() => {
+                            this.setState({ activeKey: key });
+                            console.log("Selected " + key);
+                        }} >
+                        {this.renderSubmodules(this.state.structuredObject[moduleKey])}
                     </Panel>
                 ))}
             </PanelGroup>
         )
     }
 
-    return (
-        <div>
-            <h3>Ошибки</h3>
-            {renderModules()}
-        </div>
-    );
+    render() {
+        return (
+            <div>
+                <h3>Список ошибок</h3>
+                {this.renderModules()}
+            </div>
+        )
+    };
 }
